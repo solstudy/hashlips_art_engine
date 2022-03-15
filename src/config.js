e9f1a4db87acaf9012bb76a4cdd63a7357f3a27c
lijing.quyang <lijing.quyang@bytedance.com>
2022-03-16 07:57:35 +0800

    upload config

--- src/config.js
+++ src/config.js
@@ -2,20 +2,20 @@ const basePath = process.cwd();
 const { MODE } = require(`${basePath}/constants/blend_mode.js`);
 const { NETWORK } = require(`${basePath}/constants/network.js`);
 
-const network = NETWORK.eth;
+const network = NETWORK.sol;
 
 // General metadata for Ethereum
-const namePrefix = "Your Collection";
-const description = "Remember to replace this description";
+const namePrefix = "Hashlips Learning";
+const description = "It's so wonderful to find this project";
 const baseUri = "ipfs://NewUriToReplace";
 
 const solanaMetadata = {
-  symbol: "YC",
-  seller_fee_basis_points: 1000, // Define how much % you want from secondary market sales 1000 = 10%
+  symbol: "QY",
+  seller_fee_basis_points: 100, // Define how much % you want from secondary market sales 1000 = 10%
   external_url: "https://www.youtube.com/c/hashlipsnft",
   creators: [
     {
-      address: "7fXNuer5sbZtaTEPhtJ5g5gNtuyRoKkvxdjEjEnPN4mC",
+      address: "DUHYga6LsVDgjRjCTyV4r6bYUhv3d1xKHmuuSxeN4KZh",
       share: 100,
     },
   ],
